2321253ce25130fa9d590601c69a58234c2e776f
Schreezer <96696271+Schreezer@users.noreply.github.com>
2022-12-24 19:05:19 +0530

    pass 3rd stage

--- app/main.py
+++ app/main.py
@@ -1,19 +1,29 @@
 # Uncomment this to pass the first stage
 import socket
-
+import threading
 
 def main():
     # You can use print statements as follows for debugging, they'll be visible when running tests.
     print("Logs from your program will appear here!")
 
     # Uncomment this to pass the first stage
-    #
     sock = socket.create_server(("localhost", 6379), reuse_port=True)
+    def holder(host):
+        while True:
+            try:
+                host.recv(1024)
+                host.send(b"+PONG\r\n")
+            except ConnectionError:
+                break;
+
+
+    while True:
+        host , port  = sock.accept()
+        threading.Thread(target=holder, args=(host,)).start()
+   
     # sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     # sock.connect(("localhost", 6379))
-    host , port  = sock.accept()
-    host.recv(1024)
-    host.send(b"+PONG\r\n")
+    
 
 
    
